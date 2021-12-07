2f80cc9bc1335d16c5ebb3b87b2f748434ba91fd
Jessy Naus <jessyn@spotify.com>
2021-12-07 19:50:47 +0100

    Add the Command protocol

---
+++ Sources/CommandKit/Command.swift
@@ -0,0 +1,10 @@
+/// A type that executes some work while being able to revert to the initial state.
+public protocol Command {
+
+    /// Execute the command.
+    func execute()
+
+    /// Revert the work performed by the command.
+    func undo()
+
+}
