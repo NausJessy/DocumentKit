0581f261f3fe5d2ee6b7ee1b463f4be636d70c9e
Jessy Naus <jessyn@spotify.com>
2021-12-08 08:48:42 +0100

    Update access modifiers

--- Sources/CommandKit/CompositeCommand.swift
+++ Sources/CommandKit/CompositeCommand.swift
@@ -1,27 +1,27 @@
 import Foundation
 
 /// Compose multiple commands by adding child commands.
-class CompositeCommand: Command {
+public class CompositeCommand: Command {
     /// A list of child commands.
-    private var children: [Command] = []
+    var children: [Command] = []
 
     /// Add a child command to execute.
     ///
     /// - Parameter child: The child command to execute.
-    func add(_ child: Command) {
+    public func add(_ child: Command) {
         children.append(child)
     }
 
-    func execute() {
+    public func execute() {
         children.forEach { $0.execute() }
     }
 
-    func undo() {
+    public func undo() {
         children.forEach { $0.undo() }
     }
 
     /// The number of child commands.
-    var count: Int {
+    public var count: Int {
         children.count
     }
 }
